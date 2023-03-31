a731f7da6c94705e1c83f8b0cadd491ee9282cea
Artem Terenin <artemterenin@gmail.com>
2023-03-31 22:06:56 +0300

    add page content tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,36 +1,81 @@
 import os from 'os';
-import * as fsp from 'node:fs/promises';
 import path from 'path';
 import nock from 'nock';
-import pageLoader, { getFilename } from '../src/loader.js';
+import * as fsp from 'node:fs/promises';
+import pageLoader from '../src/pageLoader.js';
+
+const getFixturePath = (...filepaths) => path.resolve(`__fixtures__/${path.join(...filepaths)}`);
+const isExist = (filepath) => {
+  const { dir, base } = path.parse(filepath);
+  return fsp.readdir(dir).then((filenames) => filenames.includes(base));
+};
+
+const pagePath = 'ru-hexlet-io-courses.html';
+const pageUrl = new URL('https://ru.hexlet.io/courses');
+const contentDir = 'ru-hexlet-io-courses_files';
+
+let expectedPage = '';
+let tempDir = '';
+let content = [
+  {
+    format: 'css',
+    url: '/assets/application.css',
+    filename: path.join(contentDir, 'ru-hexlet-io-assets-application.css'),
+  },
+  {
+    format: 'png',
+    url: '/assets/professions/nodejs.png',
+    filename: path.join(contentDir, 'ru-hexlet-io-assets-professions-nodejs.png'),
+  },
+  {
+    format: 'js',
+    url: '/packs/js/runtime.js',
+    filename: path.join(contentDir, 'ru-hexlet-io-packs-js-runtime.js'),
+  },
+  {
+    format: 'html',
+    url: '/courses',
+    filename: path.join(contentDir, 'ru-hexlet-io-courses.html'),
+  },
+];
+
+const formats = content.map(({ format }) => format);
+const scope = nock(pageUrl.origin).persist();
 
 nock.disableNetConnect();
 
-const getFixturePath = (filename) => path.resolve(`__fixtures__/${filename}`);
-
-let expected;
-let tempDir;
-
-beforeEach(async () => {
-  expected = await fsp.readFile(getFixturePath('result.html'), 'utf-8');
+beforeAll(async () => {
   tempDir = await fsp.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
-  nock('https://ru.hexlet.io').get('/courses').reply(200, expected);
+  expectedPage = await fsp.readFile(getFixturePath('expected', pagePath), 'utf-8');
+
+  const promises = content.map((file) => fsp.readFile(
+    getFixturePath('expected', file.filename),
+  ).then((data) => ({ ...file, data })));
+  content = await Promise.all(promises);
+
+  const page = await fsp.readFile(getFixturePath(pagePath), 'utf-8');
+  scope.get(pageUrl.pathname).reply(200, page);
+  content.forEach(({ url, data }) => scope.get(url).reply(200, data));
 });
 
-test('filename', () => {
-  const filename = getFilename('https://ru.hexlet.io/courses');
-  expect(filename).toBe('ru-hexlet-io-courses.html');
-});
+describe('page loader', () => {
+  test('load page', async () => {
+    await pageLoader(tempDir, pageUrl.href);
 
-test('page loader', async () => {
-  const filepath = await pageLoader(tempDir, 'https://ru.hexlet.io/courses');
-  const actual = await fsp.readFile(filepath, 'utf-8');
+    const isPageExist = await isExist(path.join(tempDir, pagePath));
+    expect(isPageExist).toBeTruthy();
 
-  expect(actual).toEqual(expected);
-});
+    const actualPage = await fsp.readFile(path.join(tempDir, pagePath), 'utf-8');
+    expect(actualPage).toEqual(expectedPage);
+  });
 
-test('page loader no such directoty error', async () => {
-  const invalidDir = '/tsettset';
-  const actual = await pageLoader(invalidDir, 'https://ru.hexlet.io/courses');
-  expect(actual).toBe(`no such directory: ${invalidDir}`);
+  test.each(formats)('load .%s file', async (format) => {
+    const { filename, data } = content.find((file) => file.format === format);
+
+    const isFileExist = await isExist(path.join(tempDir, filename));
+    expect(isFileExist).toBeTruthy();
+
+    const actualFile = await fsp.readFile(path.join(tempDir, filename));
+    expect(actualFile).toEqual(data);
+  });
 });

79632b8b7c7f7b55bc9abb0cceeeac3f96161cdd
Artem Terenin <artemterenin@gmail.com>
2023-04-02 00:59:45 +0300

    refactoring

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -8,8 +8,8 @@ program
   .version('1.0.0')
   .option('-o, --output [dir]', 'output dir', process.cwd())
   .option('-b, --progressBar [name]', 'progress bar name: default | silent', 'default')
-  .action((link, options) => {
-    pageLoader(link, options.output, options.progressBar)
+  .action((link) => {
+    pageLoader(link, program.output, program.progressBar)
       .then((result) => console.log(result))
       .catch((err) => {
         console.error(err.message);
--- src/pageLoader.js
+++ src/pageLoader.js
@@ -10,8 +10,7 @@ const log = debug('page-loader');
 const pageLoader = (link, output, progressBar) => {
   const url = new URL(link);
 
-  console.log(output, url, getFilename(url));
-  const pagePath = path.join(output ?? process.cwd(), getFilename(url));
+  const pagePath = path.join(output, getFilename(url));
   const contentDir = getContentDir(url);
   log('Input data', { url: url.href, path: pagePath, dir: output });
 
